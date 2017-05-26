ac23b5310287c22f09b24c6180b73dd9abda88b3
Erick Peirson <erick.peirson@asu.edu>
2017-05-26 09:20:06 -0400

    minor

--- builder/oauth.py
+++ builder/oauth.py
@@ -23,7 +23,8 @@ class OAuthSignIn(object):
         pass
 
     def get_callback_url(self):
-        return url_for('oauth_callback', provider=self.provider_name)
+        return url_for('oauth_callback', provider=self.provider_name,
+                       _external=True)
 
     @classmethod
     def get_provider(self, provider_name):
